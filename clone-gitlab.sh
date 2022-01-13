89ef492f9f84b0ae0862ec2b898038cbfcbc8659
Anthony Freyermuth <afreyermuth@ippon.fr>
2022-01-13 17:30:42 +0100

    Folder location bug resolved

--- clone-gitlab.sh
+++ clone-gitlab.sh
@@ -5,29 +5,45 @@ usage() {
     exit 1
 }
 
+counter=0
+
 clone() {
+    project_ids=$(curl -s --header "PRIVATE-TOKEN: $3" https://$1/api/v4/groups/$2 | jq ".projects[].id")
+    counter=$((${counter} + ${#project_ids[@]}))
+    
+    for project_id in $(curl -s --header "PRIVATE-TOKEN: $3" https://$1/api/v4/groups/$2 | jq ".projects[].id"); 
+    do
+        project=$(curl -s --header "PRIVATE-TOKEN: $3" https://$1/api/v4/projects/$project_id)
+        project_name=`echo $project | jq ".name" | tr -d '"'`
+        project_path=$(echo $project | jq ".name_with_namespace" | tr -d '"' | sed "s/ //g")
+        if [ "$4" != "" ];then
+            project_path=$(echo $project_path | sed "s/$4\///g")
+        fi
+        project_url=`echo $project | jq ".http_url_to_repo" | tr -d '"'`
+        printf "Cloning $counter projects"
+        printf "\r"
+        git clone --quiet $project_url $project_path 2>&1 | grep -v 'warning'
+    done
     groups=$(curl -s --header "PRIVATE-TOKEN: $3" https://$1/api/v4/groups/$2/subgroups | jq ".[].id")
-    if [ "$groups" == "" ]; then
-        for project_id in $(curl -s --header "PRIVATE-TOKEN: $3" https://$1/api/v4/groups/$2 | jq ".projects[].id"); 
-        do
-            project=$(curl -s --header "PRIVATE-TOKEN: $3" https://$1/api/v4/projects/$project_id)
-            project_url=`echo $project | jq ".http_url_to_repo" | tr -d '"'`
-            project_path=`echo $project | jq ".name_with_namespace" | tr -d '"' | sed 's/ //g'`
-            echo "URL PATH $project_url $project_path"
-            git clone $project_url $project_path
-        done
-    else
+    if [ "$groups" != "" ]; then
+        local_group_path=$group_path
         for group in $groups
         do
-            clone $1 $group $3
+            clone $1 $group $3 $4
         done
-    fi  
+    fi
 
 }
 
 if [[ $# -eq 3 ]]
 then
-    clone $*
+    group_name=$(curl -s --header "PRIVATE-TOKEN: $3" https://$1/api/v4/groups/$2 | jq ".name" | tr -d '"')
+    path_to_kill="$(curl -s --header "PRIVATE-TOKEN: $3" https://$1/api/v4/groups/$2 | jq ".full_name" | tr -d '"' | tr -d ' ' | sed s/.$group_name//)"
+    if [ $path_to_kill == $group_name ]; then
+        path_to_kill=""
+    fi
+    clone $* $path_to_kill
+    echo "Cloned $counter projects !"
 else
     usage
 fi
